838c9d67fb8f63f76b913ebca58b7ff93bab9feb
Gabriel Mazieri <gabrieldias7200@gmail.com>
2024-09-22 01:13:39 -0300

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
-import HeaderComponent from '@/components/Header/HeaderComponent.vue'
+import { RouterView } from 'vue-router';
+import HeaderComponent from '@/components/Header/HeaderComponent.vue';
 </script>
 
 <template>
